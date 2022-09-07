38a7cc650a2a01c122adb2d6021df4999d37ed77
Gwidas <gwidas.hiroshi@icloud.com>
2022-09-07 16:08:10 +0000

    Created a fnction to open balst.txt file

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-'Importing log file'
-f = open('blastLog.txt', 'r')
 
-for line in f:
-    print(line)
+f = open('blast.txt', 'r')
+print(f.read())
+
+
+def reader(blast):
+    with open('blast.txt') as f:
+        log = f.read()
+        print(log)
+
+
+if __name__ == '__main__':
+    reader('log')
+ 
