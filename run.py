34dc62fe87173fd7e6cab4a39e13721028957765
Gwidas <gwidas.hiroshi@icloud.com>
2022-09-09 09:22:23 +0000

    Added fucntion write and cxv file counter

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 
+import re 
+from collections import Counter
+
+
 f = open('blast.txt', 'r')
 print(f.read())
 
@@ -6,9 +10,38 @@ print(f.read())
 def reader(blast):
     with open('blast.txt') as f:
         log = f.read()
-        print(log)
+
+        regexp = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+
+        ips_list = re.findall(regexp, log)
+
+        return ips_list
+
+
+def count(ips_list):
+    return Counter(ips_list)
+
+
+def write_csv(counter):
+    with open('output.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+
+        header = ['ip', 'frequency']
+
+        writer.writeow(header)
+
+        for item in counter:
+            writer.writerow((item, counter[item]))
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    reader('log')
+    write_csv
  
